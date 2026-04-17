64302db09ec624f3aaaebd2ba9ba550a12cf22f3
Бурцев Виктор <v1to4ek@yandex.ru>
2026-04-17 03:08:17 +0300

    Пометки себе

--- ImageServer/IFileStorage.cs
+++ ImageServer/IFileStorage.cs
@@ -1,5 +1,10 @@
 ﻿namespace ImageServer
 {
+    //антипаттерн, переделать обобщенную сигнатуру
+    //обобщить типизацию данных внутри таска, а не самого таска в GetImagesUrls
+    //убрать дженерик из DownloadImageAsync - таск ниче не возвращает в принципе 
+    //переименовать методы
+    //в дальнейшем разделить интерфейс
     public interface IFileStorage<T,K> 
         where T : Task 
         where K : Task
